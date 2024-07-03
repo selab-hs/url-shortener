086101109dfd5d18e81bc0a95da90a3bdf1f161d
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-03 23:53:40 +0900

    fix : 6개월 지난 data 삭제 (1000 개 씩 청크를 나누어 삭제), NamedParameterJdbcTemplate 사용

--- src/main/java/com/urlshortener/shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/urlshortener/shortener/repository/ShortUrlRepository.java
@@ -4,10 +4,6 @@ import com.urlshortener.shortener.domain.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Transactional(readOnly = true)
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
-    List<ShortUrl> findByCreatedAtBefore(LocalDateTime date);
 }
