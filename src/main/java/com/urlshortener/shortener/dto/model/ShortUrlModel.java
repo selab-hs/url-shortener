038cc9673b76b3dd5b2b551d40d8804b5d4ec409
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-07-15 01:47:03 +0900

    fix : 로그인 인증인가 기능 수정

---
+++ src/main/java/com/urlshortener/shortener/dto/model/ShortUrlModel.java
@@ -0,0 +1,27 @@
+package com.urlshortener.shortener.dto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShortUrlModel {
+    private Long id;
+    private String originalUrl;
+    private LocalDateTime createAtl;
+
+    public static ShortUrlModel from(Long id, String originalUrl, LocalDateTime createAtl){
+
+        return ShortUrlModel.builder()
+                .id(id)
+                .originalUrl(originalUrl)
+                .createAtl(createAtl)
+                .build();
+    }
+}
